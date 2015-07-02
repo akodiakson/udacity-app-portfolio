5c019bff4cc3681b2e1d1d64a7686f8cf43ec57e
A. C. Erickson <andrewericksonisu@gmail.com>
2015-07-01 21:44:19 -0500

    --  Made artist result item tappable, to top tracks activity.

--- app/src/main/java/com/example/android/myapplication/activity/ArtistSearchResultAdapter.java
+++ app/src/main/java/com/example/android/myapplication/activity/ArtistSearchResultAdapter.java
@@ -1,12 +1,14 @@
 package com.example.android.myapplication.activity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.myapplication.R;
 import com.squareup.picasso.Picasso;
@@ -18,11 +20,10 @@ import kaaes.spotify.webapi.android.models.Image;
 
 public class ArtistSearchResultAdapter extends RecyclerView.Adapter<ArtistSearchResultAdapter.ArtistViewHolder> {
 
-    private Context context;
     private List<Artist> artistSearchResultList;
+    private View itemView;
 
     public ArtistSearchResultAdapter(Context context, List<Artist> artistSearchResultList) {
-        this.context = context;
         this.artistSearchResultList = artistSearchResultList;
     }
 
@@ -35,19 +36,27 @@ public class ArtistSearchResultAdapter extends RecyclerView.Adapter<ArtistSearch
 
     @Override
     public void onBindViewHolder(final ArtistViewHolder holder, int position) {
-        Artist artist = artistSearchResultList.get(position);
+        final Artist artist = artistSearchResultList.get(position);
         holder.artistName.setText(artist.name);
 
         List<Image> images = artist.images;
-            if(hasImages(images)){
+        Context context = holder.itemView.getContext();
+        if(hasImages(images)){
             Image firstImage = images.get(0);
             Picasso.with(context).load(firstImage.url).resize(firstImage.height, firstImage.height).centerCrop().into(holder.artistImage);
         }
 
         //TODO -- Case for no artist images, show the default image
-
-
         holder.artistImage.setBackgroundColor(context.getResources().getColor(R.color.primary_material_dark));
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = v.getContext();
+                Intent artistTopTracksIntent = new Intent(context, ArtistTopTracksActivity.class);
+                artistTopTracksIntent.putExtra(ArtistTopTracksActivity.EXTRA_ARTIST_ID, artist.id);
+                context.startActivity(artistTopTracksIntent);
+            }
+        });
     }
 
     private boolean hasImages(List<Image> images){
