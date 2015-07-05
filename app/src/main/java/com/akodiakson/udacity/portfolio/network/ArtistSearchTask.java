4abb98ac7e47c034710fe9dffa57190fd159f1cc
A. C. Erickson <andrewericksonisu@gmail.com>
2015-07-05 15:45:03 -0500

    -- Finishing touches.

--- app/src/main/java/com/akodiakson/udacity/portfolio/network/ArtistSearchTask.java
+++ app/src/main/java/com/akodiakson/udacity/portfolio/network/ArtistSearchTask.java
@@ -16,6 +16,9 @@ public class ArtistSearchTask extends AsyncTask<String, Void, ArtistsPager> {
     private static final String KEY_SEARCH_RESULT_LIMIT = "limit";
     private static final int SEARCH_RESULT_LIMIT = 10;
 
+    private static final String KEY_TYPE = "type";
+    private static final String ARTIST = "artist";
+
     private ArtistSearchTaskResultListener artistSearchTaskResultListener;
 
     public ArtistSearchTask(ArtistSearchTaskResultListener artistSearchTaskResultListener){
@@ -28,6 +31,7 @@ public class ArtistSearchTask extends AsyncTask<String, Void, ArtistsPager> {
         SpotifyService spotify = api.getService();
         Map<String, Object> options = new HashMap<>();
         options.put(KEY_SEARCH_RESULT_LIMIT, SEARCH_RESULT_LIMIT);
+        options.put(KEY_TYPE, ARTIST);
         return spotify.searchArtists(params[0], options);
     }
 
